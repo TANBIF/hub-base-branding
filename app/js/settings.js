bebce9e3c75c201b27878e1bf776bcd4ba3115ad
Meshack Junior <meshackf1@gmail.com>
2022-05-12 12:51:31 +0300

    Change default theme

--- app/js/settings.js
+++ app/js/settings.js
@@ -2,37 +2,37 @@ module.exports = {
   isDevel: true,
   inMante: false, // set to true and deploy if you want to set a maintenance message in all the services
   enabledLangs: ["en", "es", "zh", "sw"],
-  mainDomain: "l-a.site", // used for cookies (without http/https)
-  mainLAUrl: "https://l-a.site",
-  baseFooterUrl: "https://l-a.site",
-  theme: "simplex", // for now 'material', 'clean', 'superhero', 'yeti', 'cosmo', 'darkly', 'paper', 'sandstone', 'simplex', 'slate' or 'flatly' themes are available. See the last ones in: https://bootswatch.com/3/
+  mainDomain: "tanbif.costech.or.tz", // used for cookies (without http/https)
+  mainLAUrl: "https://tanbif.costech.or.tz",
+  baseFooterUrl: "https://tanbif.costech.or.tz",
+  theme: "superhero", // for now 'material', 'clean', 'superhero', 'yeti', 'cosmo', 'darkly', 'paper', 'sandstone', 'simplex', 'slate' or 'flatly' themes are available. See the last ones in: https://bootswatch.com/3/
   services: {
-    collectory: { url: "https://collections.l-a.site", title: "Collections" },
-    biocache: { url: "https://biocache.l-a.site", title: "Occurrence records" },
+    collectory: { url: "https://collections.tanbif.costech.or.tz", title: "Collections" },
+    biocache: { url: "https://biocache.tanbif.costech.or.tz", title: "Occurrence records" },
     biocacheService: {
-      url: "https://biocache-ws.l-a.site",
+      url: "https://biocache-ws.tanbif.costech.or.tz",
       title: "Occurrence records webservice",
     },
-    bie: { url: "https://species.l-a.site", title: "Species" },
+    bie: { url: "https://species.tanbif.costech.or.tz", title: "Species" },
     // This bieService var is used by the search autocomplete. With your BIE
     // service running and well indexed, you can put here your url
     bieService: {
       url: "https://bie.ala.org.au/ws",
       title: "Species webservice",
     },
-    regions: { url: "https://regions.l-a.site", title: "Regions" },
-    lists: { url: "https://lists.l-a.site", title: "Species List" },
-    spatial: { url: "https://spatial.l-a.site", title: "Spatial Portal" },
-    images: { url: "https://images.l-a.site", title: "Images Service" },
-    cas: { url: "https://auth.l-a.site", title: "CAS" },
+    regions: { url: "https://regions.tanbif.costech.or.tz", title: "Regions" },
+    lists: { url: "https://lists.tanbif.costech.or.tz", title: "Species List" },
+    spatial: { url: "https://spatial.tanbif.costech.or.tz", title: "Spatial Portal" },
+    images: { url: "https://images.tanbif.costech.or.tz", title: "Images Service" },
+    cas: { url: "https://auth.tanbif.costech.or.tz", title: "CAS" },
   },
   otherLinks: [
-    { title: "Datasets", url: "https://collections.l-a.site/datasets" },
+    { title: "Datasets", url: "https://collections.tanbif.costech.or.tz/datasets" },
     {
       title: "Explore your area",
-      url: "http://biocache.l-a.site/explore/your-area/",
+      url: "http://biocache.tanbif.costech.or.tz/explore/your-area/",
     },
-    { title: "Datasets", url: "https://collections.l-a.site/datasets" },
+    { title: "Datasets", url: "https://collections.tanbif.costech.or.tz/datasets" },
     { title: "twitter", url: "", icon: "twitter" },
   ],
 };
